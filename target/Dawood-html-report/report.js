54db92ccad63393749dbe46137d8e862911de511
Muhammad Dawood Khan <muhammad.khan@aricent.com>
2016-06-22 17:03:35 +0100

    New Commit

---
+++ target/Dawood-html-report/report.js
@@ -0,0 +1,327 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("HomePage.feature");
+formatter.feature({
+  "line": 2,
+  "name": "As a user I want to go to the homepage",
+  "description": "So that i can see all the deals and options available there",
+  "id": "as-a-user-i-want-to-go-to-the-homepage",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@homepage"
+    }
+  ]
+});
+formatter.before({
+  "duration": 18511657400,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 5,
+  "name": "Opening homepage",
+  "description": "",
+  "id": "as-a-user-i-want-to-go-to-the-homepage;opening-homepage",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 6,
+  "name": "I have open the browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I enter the web address of the website",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "I am able to see the homepage",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageSteps.i_have_open_the_browser()"
+});
+formatter.result({
+  "duration": 148095640,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_enter_the_web_address_of_the_website()"
+});
+formatter.result({
+  "duration": 23205,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_am_able_to_see_the_homepage()"
+});
+formatter.result({
+  "duration": 20527,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 99958,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 12687530260,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "Opening of hotel search page",
+  "description": "",
+  "id": "as-a-user-i-want-to-go-to-the-homepage;opening-of-hotel-search-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 12,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 13,
+  "name": "I go to the hotel option",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "I successfully navigate to hotel search page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageSteps.i_am_on_homepage()"
+});
+formatter.result({
+  "duration": 5261992194,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_go_to_the_hotel_option()"
+});
+formatter.result({
+  "duration": 21420,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_successfully_navigate_to_hotel_search_page()"
+});
+formatter.result({
+  "duration": 32129,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 17404,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 16047370363,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 17,
+  "name": "Opening of Flight search page",
+  "description": "",
+  "id": "as-a-user-i-want-to-go-to-the-homepage;opening-of-flight-search-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 18,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 19,
+  "name": "I go to the hotel option",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 20,
+  "name": "I successfully navigate to Flight search page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageSteps.i_am_on_homepage()"
+});
+formatter.result({
+  "duration": 8134542382,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_go_to_the_hotel_option()"
+});
+formatter.result({
+  "duration": 38823,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_successfully_navigate_to_Flight_search_page()"
+});
+formatter.result({
+  "duration": 22759,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 20973,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 54702834127,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 23,
+  "name": "Opening of Flight and Hotel search page",
+  "description": "",
+  "id": "as-a-user-i-want-to-go-to-the-homepage;opening-of-flight-and-hotel-search-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 24,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 25,
+  "name": "I go to the hotel option",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 26,
+  "name": "I successfully navigate to Flight and Hotel package search page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageSteps.i_am_on_homepage()"
+});
+formatter.result({
+  "duration": 8283232862,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_go_to_the_hotel_option()"
+});
+formatter.result({
+  "duration": 33022,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_successfully_navigate_to_Flight_and_Hotel_package_search_page()"
+});
+formatter.result({
+  "duration": 32129,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 26328,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 12960791848,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 29,
+  "name": "Opening of Car search page",
+  "description": "",
+  "id": "as-a-user-i-want-to-go-to-the-homepage;opening-of-car-search-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 30,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 31,
+  "name": "I go to the hotel option",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 32,
+  "name": "I successfully navigate to Car search page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageSteps.i_am_on_homepage()"
+});
+formatter.result({
+  "duration": 6007428589,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_go_to_the_hotel_option()"
+});
+formatter.result({
+  "duration": 15619,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_successfully_navigate_to_Car_search_page()"
+});
+formatter.result({
+  "duration": 29898,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 19634,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 13801578536,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 35,
+  "name": "Opening of Deal search page",
+  "description": "",
+  "id": "as-a-user-i-want-to-go-to-the-homepage;opening-of-deal-search-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 36,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 37,
+  "name": "I go to the hotel option",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 38,
+  "name": "I successfully navigate to Deal search page",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageSteps.i_am_on_homepage()"
+});
+formatter.result({
+  "duration": 4835874011,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_go_to_the_hotel_option()"
+});
+formatter.result({
+  "duration": 12941,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.i_successfully_navigate_to_Deal_search_page()"
+});
+formatter.result({
+  "duration": 15172,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 17404,
+  "status": "passed"
+});
+});
